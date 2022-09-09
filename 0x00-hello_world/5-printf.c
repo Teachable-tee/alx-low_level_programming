a71b4bd73b6ee65e017563cd5c8235635aa0a266
Oluwatosin <jinaduoluwatosin@gmail.com>
2022-09-08 19:49:50 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print the sting in the printf function
+ *
+ * Description: using the main function
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
