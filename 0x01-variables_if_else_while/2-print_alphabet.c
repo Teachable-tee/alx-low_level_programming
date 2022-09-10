d4e6f64735ff4f4db1f8d617d5f0fcec7836374f
Oluwatosin <jinaduoluwatosin@gmail.com>
2022-09-09 17:51:34 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - print if the last digit is greater than 5, zero, less than 6
+ *
+ * Description: using the main function
+ * this programming prints "Programming is positive, zero or negative"
+ * Return: 0
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'a' ; n <= 'z' ; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
