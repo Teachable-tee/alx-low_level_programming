b892a497d08b62eb6924e34ea41c9730d3554f2e
Oluwatosin <jinaduoluwatosin@gmail.com>
2022-09-09 18:16:25 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - print all single digit mumbers of base 10
+ *
+ * Description: using the main function
+ * this programming prints "Programming is positive, zero or negative"
+ * Return: 0
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '0' ; n <= '9' ; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
