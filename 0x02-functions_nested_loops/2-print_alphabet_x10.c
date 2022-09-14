2f37a71b29fe7b60e0fc748cb36ce7d773110b14
Oluwatosin <jinaduoluwatosin@gmail.com>
2022-09-13 19:39:17 -0700

    alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 {
 	char c, i;
 
-	for (i = 0; i <= 9; c++)
+	for (i = 0; i <= 9; i++)
 
 	{
 		for (c = 'a'; c <= 'z'; c++)
@@ -19,4 +19,5 @@ void print_alphabet_x10(void)
 		}
 		_putchar('\n');
 	}
+_putchar('\n');
 }
