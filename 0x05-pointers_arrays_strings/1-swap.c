8b6d87b38a6755304aad2165e9708cccf5e418f6
Oluwatosin <jinaduoluwatosin@gmail.com>
2022-09-19 09:55:04 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the values of two integers
+ * @a: first number
+ * @b: second number
+ * Return: nothing to return
+ */
+
+void swap_int(int *a, int *b)
+{
+	swap_int (*a, *b);
+}
