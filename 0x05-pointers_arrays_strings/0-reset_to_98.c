640cff5320b80e2f4ef0e869e7c708d5b3266347
Oluwatosin <jinaduoluwatosin@gmail.com>
2022-09-19 09:23:42 -0700

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - point an int as parameter and
+ * updates the value to 98
+ * @*n: parameter
+ *
+ * Return: nothing to return
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+a function that takes a pointer to an int as parameter and updates the value it points to 98
+a function that swap the value of two integer
