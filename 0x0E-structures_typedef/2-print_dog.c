7afd3089ae95dcff6d0ac2ea1dfc7c08a163e3a6
Oluwatosin <jinaduoluwatosin@gmail.com>
2022-10-10 15:23:00 -0700

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,17 +13,17 @@ void print_dog(struct dog *d)
 		return;
 
 	if (d->name == NULL)
-		printf("Name: (nll\n");
+		printf("Name: (nll)\n");
 	else
 		printf("Name: %s\n", d->name);
 
 	if (d->age > 0)
-		printf("Age: (nll\n");
+		printf("Age: (nll)\n");
 	else
 		printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
-		printf("Owner: (nll\n");
+		printf("Owner: (nll)\n");
 	else
 		printf("Owner: %s\n", d->owner);
 }
