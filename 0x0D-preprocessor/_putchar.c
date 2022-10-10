933c99d8833808ef016a4dc46eadf8af60579568
Oluwatosin <jinaduoluwatosin@gmail.com>
2022-10-10 11:57:32 -0700

    task 0

---
+++ 0x0D-preprocessor/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to prints
+ *
+ * Return: on sucess 1
+ * on error -1 is returned, and error is set appropriately
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
